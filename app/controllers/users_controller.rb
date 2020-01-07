851cd0cffa6a0c80cad7f82f3d53ed5865b76ced
cep104 <candicelizabethpeters@gmail.com>
2020-01-07 12:28:14 -0600

    AFTER REVIEW

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,20 @@
 class UsersController < ApplicationController
+    get '/signup' do 
+        if Helpers.is_logged_in?(session)
+           redirect '/tweets'
+        else 
+           erb :'users/signup'
+        end
+    end
 
+    post '/signup' do
+        if !(params.has_value?(""))
+          user = User.create(params)
+          session["user_id"] = user.id
+          redirect '/tweets'
+        else
+          redirect '/signup'
+        end
+      end
 
 end
